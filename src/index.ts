0966e82bc155db0139e3995fe2b66d45215eb85e
Bertalan Körmendy <korber314@gmail.com>
2019-01-08 12:04:12 +0100

    added npm script for env dependent server start

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { prisma } from './generated/prisma-client'
-import { resolvers } from './resolvers'
-import { permissions } from './permissions'
+import { GraphQLServer } from "graphql-yoga";
+import { prisma } from "./generated/prisma-client";
+import { resolvers } from "./resolvers";
+import { permissions } from "./permissions";
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
+  typeDefs: "src/schema.graphql",
   resolvers: resolvers as any,
   middlewares: [permissions],
   context: request => {
     return {
       ...request,
-      prisma,
-    }
-  },
-})
-
-server.start(() => console.log('Server is running on http://localhost:4000'))
+      prisma
+    };
+  }
+});
+server.start(() => console.log("Server is running on http://localhost:4000"));
