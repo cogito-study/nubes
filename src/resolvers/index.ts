f56f63bedde562188a668a0cd9924aa8057ea557
Bertalan Körmendy <korber314@gmail.com>
2019-02-17 14:17:40 +0100

    fix typo

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,20 +1,21 @@
 import { Resolvers } from '../generated/graphqlgen';
-
-import { Query } from './Query';
-import { User } from './User';
+import { AuthPayload } from './AuthPayload';
+import { Comment } from './Comment';
+import { CommentPreviousValues } from './CommentPreviousValues';
+import { FileUploadPayload } from './FileUploadPayload';
+import { Institute } from './Institute';
+import { Mutation } from './Mutation';
 import { Note } from './Note';
+import { Query } from './Query';
 import { Subject } from './Subject';
 import { SubjectInfo } from './SubjectInfo';
-import { Comment } from './Comment';
-import { Mutation } from './Mutation';
-import { AuthPayload } from './AuthPayload';
-import { FileUploadPayload } from './FileUploadPayload';
-import { CommentPreviousValues } from './CommentPreviousValues';
+import { User } from './User';
 
 export const resolvers: Resolvers = {
   Query,
   User,
   Note,
+  Institute,
   Subject,
   SubjectInfo,
   Comment,
